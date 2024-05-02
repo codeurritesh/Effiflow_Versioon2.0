af69d3c677338d03fadb76df0c8fc31c3c6f5809
Shashank Saraswat <shashanksaraswat@Shashanks-MacBook-Air.local>
2024-05-03 00:19:55 +0530

    fix the email issue

--- server/html-template/send-otp-template.js
+++ server/html-template/send-otp-template.js
@@ -1,13 +1,26 @@
 module.exports.sendOTPTemplate = (otp, title) => (`
-  <body style="width: 100%;">
-  <div style="background: blueviolet; text-align: center;">
-    <h1 style="font-family: sans-serif;color: #fff;">
-      TOTE<span style="color: yellowgreen;">WEB</span>
-    </h1>
+<!DOCTYPE html>
+<html>
+<head>
+  <base target="_top">
+</head>
+<body>
+  <div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
+  <div style="margin:50px auto;width:80%;padding:20px 0">
+  <div style="border-bottom:5px solid #eee">
+    <a href="" style="font-size:30px;color: #f7c800;text-decoration:none;font-weight:600">EffiFlow</a>
   </div>
-  <div style="font-family: sans-serif; text-align: center">
-    <h4 style="text-transform: uppercase;letter-spacing: 1px;">${title}</h4>
-    <h2 style="text-align: center;letter-spacing: 4px;font-size: 40px;">${otp}</h2>
+  <p style="font-size:15px">Hello User,</p>
+  <p>Thank you for choosing EffiFlow. Use this OTP to complete your Sign Up procedures and verify your account on EffiFlow.</p>
+  <p>Remember, Never share this OTP with anyone.</p>
+  <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${otp}</h2>
+  <p style="font-size:15px;">Regards,<br />Team EffiFlow</p>
+  <hr style="border:none;border-top:5px solid #eee" />
+  <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
+    <p>EffiFlow Inc</p>
   </div>
-  </body>
+  </div>
+  </div>
+</body>
+</html>
 `);
